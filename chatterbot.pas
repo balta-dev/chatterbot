a6fdaa8cbc8e6d60299933acc74f6978f9eb0f2e
balta-dev <88515914+balta-dev@users.noreply.github.com>
2024-06-01 14:09:24 -0300

    new system implementation
    chatterbot was mostly redone, remade how it detects keywords (added functionality), new more dynamic chat sessions (modern chatting style replica like, taking into account Pascal limitations). Instead of using static memory (vectors) to temporarely store all data files, now it's using dynamic memory as singly-linked lists. music was compressed to 50% of its original quality to reduce project size, added more believable "8 bit" style sound.

--- chatterbot.pas
+++ chatterbot.pas
@@ -1,100 +1,272 @@
-program chatterbot;	
+program chatterbot;
+{$codepage UTF-8}
 
-uses crt, SysUtils, Keyboard, MMSystem, StrUtils,
+uses handler in '.units/misc/handler.pas', 
+lista in '.units/misc/lista.pas', 
+sysutils, 
+crt, 
+keyboard, 
+visuales in '.units/screen/visuales.pas', 
+configuraciones in '.units/config/configuraciones.pas', 
+dia in '.units/misc/dia.pas', 
+music in '.units/misc/music.pas', 
+menuPrincipal in '.units/screen/menuPrincipal.pas', 
+paginas in '.units/screen/paginas.pas', 
+respuestas in '.units/screen/respuestas.pas';
 
-configuraciones in 'units/configuraciones.pas',
-procesamiento in 'units/procesamiento.pas',
-historial in 'units/historial.pas',
-principal in 'units/principal.pas',
-administrador in 'units/administrador.pas',
-borrarEspacios in 'units/borrarEspacios',
-box in 'units/box',
-music in 'units/music',
-dia in 'units/dia';
-type vectorEspecial2 = array [1..maxSongs] of string;
-
-procedure textWrite();
-var texto:ansiString;
-int30:integer;
-completition_type:boolean;
+procedure escribirPrompt(var entrada:string); //limita la longitud de escritura para evitar bugs
+var caracter: char;
+guardarX:byte;
 begin
 
-	int30:=30;
-	boxDO(1, 115, 6, 25, '*', false);
-	texto := 'Lionel Andres Messi es indiscutiblemente uno de los mejores jugadores de la historia, con 42 titulos oficiales a lo largo de su carrera; 35 con el FC Barcelona, 2 con el PSG, 3 con la seleccion Argentina, 1 con la seleccion Argentina sub-20 y otro con la sub-23. En lo que titulos individuales respecta, Messi posee 9 MVP de la Liga, 8 Pichichis de la Liga, 7 Balones de Oro, 6 Botas de Oro, 6 Maximo goleador de la Champions, 4 Onze d"Or, 2 Mejor Jugador de Europa, 1 MVP Mundial 2022, 1 Premio The Best, 1 FIFA World Player, 1 Balon de Oro Mundial 2014, 1 MVP Copa America 2021, 1 FIFA FIFPro, 1 Premio Laureus, 1 Golden Boy y 1 Trofeo Bravo.';
-	completition_type := true;
-	gotoxy(18, 10);
-	animation(texto, int30, completition_type);
-	readkey;
+
+	restaurarBoxPrompt;
+	DoneKeyboard;
+
+	caracter := #0;
+	entrada := '';
+
+	 repeat
+
+	 	caracter := readkey;
+
+		if (caracter <> #13) and (caracter <> #8) and (caracter <> #27) then write(caracter);	//----- Distinto de Enter y Backspace --------//
+
+		if (caracter <> #8) then 
+		begin 
+		entrada := entrada + caracter;	//----- Distinto de Backspace -------//
+		if whereX > 94 then 
+			begin 
+			gotoxy(whereX-1, 27);
+			delete(entrada, length(entrada), 1);
+			end;
+		end
+		else if (caracter <> #27) then
+		begin
+
+			if whereX > 9 then gotoxy(whereX-1, 27);
+			delete(entrada, length(entrada), 1);
+			ClrEol;
+			guardarX := whereX;
+			gotoxy(118, 27); writeln(')');
+			gotoxy(guardarX, 27);
+
+		end;
+
+		if (caracter = #27) then
+		begin
+
+			if whereX > 9 then gotoxy(whereX, 27);
+			delete(entrada, length(entrada), 1);
+			caracter := #13;
+			entrada := '';
+
+		end;
+	 	
+	 until caracter=#13;
+
+	 if length(entrada) = 1 then entrada := '';
+
+	InitKeyboard;
+	restaurarBoxPrompt;
+	base;
+	
+end;
+
+procedure chat;
+var K: TKeyEvent;
+entrada, entradaAux:string;
+respuesta:tDatoLista;
+cantidadLineas, posicionYbot, posicionYuser, paginaActual, ultimaPagina, colorTexto:byte;
+lista, sublista:tLista;
+infoConfig, tiempo:tVector;
+forzarCierre, guardarChat, autoGuardadoActivado, velocidadTexto, musicaActiva:boolean;
+
+begin
+
+	crearLista(lista);
+	leerConfiguracionesGuardadas(infoConfig, colorTexto, guardarChat, autoGuardadoActivado, velocidadTexto, musicaActiva); forzarCierre:=false;
+	decodificarTiempo(tiempo);
+
+	InitKeyboard;
+	boxPrompt;
+
+	posicionYuser := 5; posicionYbot := posicionYuser+4;
+	paginaActual := 1;
+	ultimaPagina := 1;
+	K:= 0;
+
+	gotoxy(90,1);
+	writeln('PAGINA: ', paginaActual, '/', ultimaPagina);
+
+    while (TKeyRecord(K).KeyCode<>283) and (not forzarCierre) do
+	begin
+
+		base;
+
+		K:= GetKeyEvent;
+		K:= TranslateKeyEvent(K);
+
+		gotoxy(90,1);
+		writeln('PAGINA: ', paginaActual, '/', ultimaPagina);
+
+		case TKeyRecord(K).KeyCode of
+		65313: subirPagina(paginaActual, ultimaPagina, lista, infoConfig, velocidadTexto);
+		65319: bajarPagina(paginaActual, ultimaPagina, lista, infoConfig, velocidadTexto);
+		7181: begin
+
+				if paginaActual <> ultimaPagina then //por si decis revisar chat viejo. si no es el caso, esto no se ejecuta.
+				begin 
+					clrscr;
+					devolverSublista(lista, ultimaPagina, sublista, velocidadTexto);
+					restaurarPagina(sublista, infoConfig, velocidadTexto);
+					paginaActual := ultimaPagina; 
+				end;
+
+				escribirPrompt(entrada);
+				entradaAux := entrada;
+				entrada := lowercase(entrada);
+
+				if (entrada <> '') then
+				begin
+
+						checkPantallaLlena(paginaActual, ultimaPagina, posicionYuser, posicionYbot, lista);
+						textoUsuario(lista, posicionYuser, entradaAux, infoConfig[1]);
+						
+						getResponse(entrada, respuesta);
+						respuestasEspeciales(entrada, respuesta, forzarCierre, guardarChat);
+
+						textoBot(lista, cantidadLineas, posicionYbot, respuesta, velocidadTexto);
+
+						posicionYuser := posicionYbot+cantidadLineas+2;
+						posicionYbot := posicionYuser+4;
+
+				end;
+
+				InitKeyboard;
+
+		    end;
+
+	    end;
+
+	end;
+
+	DoneKeyboard;
+	if forzarCierre then readkey;
+
+	if guardarChat then guardarChatHandler(lista, tiempo, autoGuardadoActivado);
+	eliminarLista(lista);
 
 end;
 
-procedure mainProgram();
-var configuration:textfile;
-infoConfig:vectorConf;
-colorTextoBYTE:byte;
-colorTextoSTR:string;
-saltearMenu:boolean;
-autoSave:boolean;
-completition_type:boolean;
-username:string;
-save:boolean;
-auxDate:string;
-auxTime:string;
-v:t_vector2;
-m:byte;
-musicON:boolean;
-listaCanciones:vectorEspecial2;
-index:byte;
-Cmd:pchar;
-StopSong:pchar;
-songName:string;
+procedure chequearArchivoData(var hayData:boolean);
+var archivo:textfile;
+begin
 
+	assign (archivo, '.\data\datos.txt');
+	{$I-}
+	reset(archivo);
+	{$I+}
+	if IOResult = 0 then hayData := true
+	else hayData:=false;
+
+end;
+
+procedure ifChat(hayData:boolean);
+begin
+
+	if hayData then chat
+	else begin
+
+		gotoxy(25,22);
+		writeln('¡No existe un archivo de datos! Si no lo borraste, ');
+		gotoxy(25,23);
+		write('entra a "Administrador" y agrega tus propias palabras claves.. o reinstalame :(');
+		readkey;
+
+	end;
+
+end;
+
+procedure menu(infoConfig:tVector; songName:string; Cmd:pchar; StopSong:pchar);
+var op:string;
+colorTexto:byte;
+guardarChat, autoGuardadoActivado, velocidadTexto, musicaActiva:boolean;
+hayData:boolean;
 
 	begin
 
-		//musicFinding(listaCanciones, index, Cmd, songName);
+		repeat
 
-		saltearMenu:=false;////
-		autoSave:=false;
-		save:=false;
-		completition_type:=true;
-		musicON:=true;
-		colorTextoBYTE := 14;						//DEFAULT PRESET
-		colorTextoSTR:='Amarillo';
-		username:=GetUserDir;
-		delete(username, 1, 9);
-		delete(username, length(username), length(username));
+			leerConfiguracionesGuardadas(infoConfig, colorTexto, guardarChat, autoGuardadoActivado, velocidadTexto, musicaActiva);
+			hayData:=false;
+			clrscr;
+			
+			chequearArchivoData(hayData);
 
-		If Not DirectoryExists('chatLog') then CreateDir('chatLog');
+			crearDibujo;
+			box(25, 97, 8, 20, '.');
+			gotoxy(3, 29);
+			if (songName <> '') and (musicaActiva) then writeln('Sonando: ' + songName);
+			gotoxy(25,7);
+			write('Bienvenid@, ');
+			textcolor(colorTexto);
+			write(infoConfig[1], '!');
+			textcolor(white);
+			gotoxy(47, 10);
+			textcolor(colorTexto);
+			writeln('---- FOOTBALL CHATTERBOT ----');
+			textcolor(white);
+			gotoxy(79, 10);
+			writeln('Opcion: ');
+			gotoxy(56, 12);
+			writeln('1) Iniciar');
+			gotoxy(52, 14);
+			writeln('2) Configuraciones');
+			gotoxy(53, 16);
+			writeln('3) Administrador');
+			gotoxy(57,18);
+			writeln('4) Salir');
+			gotoxy(87, 10);
+			readln(op);
 
-		If Not DirectoryExists('music') then CreateDir('music');
+			case op of
+			'1': begin mostrarSeleccion(52, 12, colorTexto); ifChat(hayData); end; 
+			'2': begin mostrarSeleccion(48, 14, colorTexto); configMenu(StopSong, Cmd, songName); end; 
+			'3': begin mostrarSeleccion(49, 16, colorTexto); adminMenu;  end;
+			'4': begin gotoxy(50, 22); animation('Adios, nos vemos pronto!', 30, velocidadTexto); mostrarSeleccion(53, 18, colorTexto); end;
+			end;
 
-		configOpeningHandling(username, configuration, infoConfig, colorTextoSTR, colorTextoBYTE, saltearMenu, autoSave, completition_type, musicON);
-		logOpeningHandling(auxDate, auxTime);
-		configClosingHandling(autoSave, configuration, infoConfig, save, musicON);
+		until op='4';
 
-		//if musicON then SendMCICommand(Cmd);
-		//clrscr;
+end;
 
-		musicFinding(listaCanciones, index, Cmd, songName, StopSong);
-		if musicON then SendMCICommand(Cmd);
+procedure main;
+var infoConfig:tVector;
+listaCanciones:tVectorCanciones;
+index:byte;
+Cmd, StopSong:pchar;
+songName:string;
 
-		if not saltearMenu then start(colorTextoBYTE, completition_type, songName, musicON, Cmd, StopSong);
+begin
 
-		menu(username, configuration, infoConfig, colorTextoSTR, colorTextoBYTE, saltearMenu, autoSave, completition_type, save, v, m, musicON, songName, Cmd, StopSong, listaCanciones);	
+	configOpeningHandling(infoConfig);
 
-		configClosingHandling(autoSave, configuration, infoConfig, save, musicON);
-		logClosingHandling(v, auxDate, auxTime, autoSave, m, save);
+	If Not DirectoryExists('music') then CreateDir('music');
+	If Not DirectoryExists('data') then CreateDir('data');
+	If Not DirectoryExists('.units') then CreateDir('.units');
+	musicFinding(listaCanciones, index, Cmd, songName, StopSong);
 
-		//SendMCICommand('close overlay');
-		
-	end;
+	if infoConfig[6]='Activada' then SendMCICommand(Cmd);
+	if infoConfig[3]='Desactivado' then pantallaPrincipal(songName); //si esta desactivado, saltear pantalla principal
 
+	menu(infoConfig, songName, Cmd, StopSong);
 
-begin  	//PROGRAMA PRINCIPAL
+end;
 
-	mainProgram;
-	//textWrite;
+begin
+
+	randomize;
+	main;
 
 end.
